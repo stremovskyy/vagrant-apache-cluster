93e1e2f615cd0f2345589ea296d0a45f5f15c077
Anthony <stremovskyy@me.com>
2018-09-07 14:40:15 +0300

    deleted old file

--- scripts/hosts-file-setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "hosts file setup..."
-
-sudo echo "192.168.1.210 node1" | sudo tee -a /etc/hosts
-sudo echo "192.168.1.220 node1" | sudo tee -a /etc/hosts
-sudo echo "192.168.1.230 node3" | sudo tee -a /etc/hosts
-

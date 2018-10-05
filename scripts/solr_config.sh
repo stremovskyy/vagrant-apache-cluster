bb7f4d62f2c82daeec1940aef7d7ea99fdbe7f1a
Anthony <stremovskyy@me.com>
2018-10-05 10:47:23 +0300

    Solr config fix

--- scripts/solr_config.sh
+++ scripts/solr_config.sh
@@ -4,7 +4,7 @@ echo "Configuring Apache Solr"
 
 nohup $SOLR_NAME/bin/solr zk mkroot /solr -z $ZK_NODES &
 
-cat > $SOLR_NAME/security.json <<EOL
+tee -a $SOLR_NAME/security.json <<EOL
 {
   "authentication":{
    "blockUnknown": false,
@@ -23,6 +23,17 @@ cat > $SOLR_NAME/security.json <<EOL
 }
 EOL
 
+sudo yum -y install lsof
 nohup $SOLR_NAME/bin/solr zk cp file:$SOLR_NAME/security.json zk:/security.json -z $ZK_NODES &
 nohup $SOLR_NAME/bin/solr zk cp file:$SOLR_NAME/server/solr/solr.xml  zk:/solr.xml -z $ZK_NODES &
 nohup $SOLR_NAME/bin/solr create -c vagrant_index -replicationFactor 3 -shards 1 &
+sudo sed -i '$ a * soft nofile 65000' /etc/security/limits.conf
+sudo sed -i '$ a * hard nofile 65000' /etc/security/limits.conf
+sudo sed -i '$ a * soft nproc 65000' /etc/security/limits.conf
+sudo sed -i '$ a * hard nproc 65000' /etc/security/limits.conf
+ulimit -u 65000
+sudo sysctl -p /etc/sysctl.conf
+sudo sysctl -w fs.file-max=100000
+sudo sysctl --system
+
+echo "DONE Configuring Apache Solr"
